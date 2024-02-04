321cb33ea483b86533343ab79732b021cce2c25b
Alfonsina <alfonsina.l@gmail.com>
2024-02-04 17:59:40 -0300

    feat: agrega colores a configuracion de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "#4ac9b4",
+      secondary: "#2a5851",
+      // ...
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
