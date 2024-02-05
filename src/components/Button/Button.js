6bdb28bf5356e863ef7988500dc0b6ce3cce11bf
Felipe Torres <felipe.torressepulveda@gmail.com>
2024-02-04 16:20:19 -0800

    TEST

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -17,27 +17,7 @@ export const Button = ({
 
   const className = `min-w-40 md:min-w-56 ${variantColors} rounded-xl text-center px-4 py-2 text-2xl md:text-3xl font-semibold`;
 
-  if (href?.match(/\s(http[^\s]+)/i)) {
-    return (
-      <a id={id} className={className} href={href} target={target}>
-        {children}
-      </a>
-    );
-  }
-
-  if (href) {
-    return (
-      <Link id={id} className={className} href={href}>
-        {children}
-      </Link>
-    );
-  }
-
-  return (
-    <button id={id} onClick={onClick} className={className}>
-      {children}
-    </button>
-  );
+  return <div>asdjohadads</div>
 };
 
 Button.propTypes = {
