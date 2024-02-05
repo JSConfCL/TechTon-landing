d094e82e245b290c1c717b1d27cbdafbd18efe6e
Rodrigo Bustamante <rbustamantejelvez@gmail.com>
2024-02-04 22:07:12 -0300

    feat: add site container in layout component

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <section className="flex flex-col items-center justify-between">
       <h1>TechTon</h1>
-    </main>
+    </section>
   );
 }
