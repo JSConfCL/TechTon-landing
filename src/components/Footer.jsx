39759248ac7924ac734240b76534d660b15db560
daraletdev <daraletdev@gmail.com>
2024-02-05 15:41:44 -0300

    feat: Create Footer and add to home (#12)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,16 +11,16 @@ const Footer = () => {
                     <Image alt='Dibujo de solidaridad' src='/footer/icon-hands.svg' height={25} width={50} style={{height:'10em'}} className='w-full' />
                 </div>
                 {/* Elemento del medio */}
-                <div className='flex-1 flex flex-col items-center'>
-                    <div className='sm:mb-3 text-md font-semibold bg-white px-5 text-center rounded-b-xl sm:rounded sm:text-md p-3 md:p-4'>
+                <div className='m-auto flex-2 flex flex-col items-center'>
+                    <div className='sm:mb-3 text-lg sm:text-md font-semibold bg-white md:p- px-5 text-center rounded-b-xl sm:rounded p-3 md:p-4'>
                         <span className='font-black'>09 y 10</span> de Febrero de 2024
                     </div>
-                    <div className='py-5 sm:py-0 space-y-2'>
-                        <div className='text-sm sm:text-xs flex items-center md:text-[15px]'>
+                    <div className='py-5 sm:py-0 space-y-3'>
+                        <div className='text-md sm:text-sm flex items-center md:text-[15px]'>
                             <span className='font-bold mr-1'>Viernes 09</span> desde las 14:00 hrs.
                             <Image className='ml-2' alt='Hora Chile' src='/footer/chilean-flag.svg' height={20} width={20} />
                         </div>
-                        <div className='text-sm sm:text-xs flex items-center md:text-[15px]'>
+                        <div className='text-md sm:text-sm flex items-center md:text-[15px]'>
                             <span className='font-bold mr-1'>Sábado 10</span> desde las 9:00 hrs.
                             <Image className='ml-2' alt='Hora Chile' src='/footer/chilean-flag.svg' height={20} width={20} />
                         </div>
@@ -42,12 +42,12 @@ const Footer = () => {
                 </div>
                 <div className='flex-1 flex justify-around'>
                     {/* Día */}
-                    <div className='flex items-center text-lg'>
-                        <Image alt='' src='/footer/icon-calendar.svg' className='mr-2' height={50} width={50} />
+                    <div className='flex items-center text-xl'>
+                        <Image alt='' src='/footer/icon-calendar.svg' className='mr-5' height={50} width={50} />
                         <span className='font-black mr-1'>09 y 10</span> de Febrero de 2024
                     </div>
                     {/* Hora */}
-                    <div className='gap-5 flex items-center text-lg'>
+                    <div className='gap-10 flex items-center text-xl'>
                         <Image alt='' src='/footer/icon-clock.svg' height={50} width={50} />
                         <div className='flex flex-col space-y-4'>
                             <div className='flex'>
