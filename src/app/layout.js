139afa56aa6888acc91f24962e3ddc0bd4ca01d4
Denisse Alfaro <83615859+daraletdev@users.noreply.github.com>
2024-02-05 16:09:55 -0300

    feat: Create Twitch Button Component and Add to Home (#11) (#41)
    Closes #41

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import { Inter, Kufam, MuseoModerno } from "next/font/google";
 
 import "./globals.css";
+import TwitchButton from "@/components/TwitchButton";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,7 +20,8 @@ const museo_moderno = MuseoModerno({
 
 export const metadata = {
   title: "TechTon | JsChile",
-  description: "Recaudaremos fondos que serán entregados a Bomberos de Chile para ayudar a mitigar los efectos causados por los incendios que comenzaron a ocurrir el 02-02-2024 en la V Región de Chile",
+  description:
+    "Recaudaremos fondos que serán entregados a Bomberos de Chile para ayudar a mitigar los efectos causados por los incendios que comenzaron a ocurrir el 02-02-2024 en la V Región de Chile",
 };
 
 export default function RootLayout({ children }) {
@@ -31,6 +33,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className} min-h-screen bg-[#232121] text-white`}
       >
+        <div className="hidden lg:block">
+          <TwitchButton />
+        </div>
         {/* TODO: Put the twitch button on the right of the screen in desktop here! */}
         <main className="px-8 lg:px-64 py-12 lg:py-24">{children}</main>
         {/* TODO: Put the footer here! */}
