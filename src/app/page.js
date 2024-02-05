d5969d9c34a9a7cb1094e9cff1f902bbfacaf31b
Francisca Beatriz Medina Concha <fbmedinaconcha@gmail.com>
2024-02-05 00:52:22 -0300

    Update page.js with donate and info button (#23)

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import { Button } from '../components/Button/Button.js';
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-between">
-      <h1>TechTon</h1>z
+      <h1>TechTon</h1>
+
+      <div className="flex gap-12">
+        <Button id="boton-donate" href="#" variant="primary">
+          Donar
+        </Button>
+        <Button id="boton-info" href="#" variant="secondary">
+          Saber más
+        </Button>
+      </div>
     </section>
   );
 }
