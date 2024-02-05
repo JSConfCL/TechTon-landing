5434862df8be3a3eba5fe9279c9ece3beb5c2449
Francisca Medina Concha <fbmedinaconcha@gmail.com>
2024-02-05 01:27:49 -0300

    Base of subtitle

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Button } from '../components/Button/Button.js';
+import { Subtitle } from '../components/Subtitle/Subtitle.js';
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-between">
       <h1>TechTon</h1>
+      <Subtitle></Subtitle>
 
       <div className="flex gap-12">
         <Button id="boton-donate" href="#" variant="primary">
---
+++ src/components/Subtitle/Subtitle.js
@@ -0,0 +1,10 @@
+export function Subtitle() {
+    return (
+      <h3 className="flex items-center justify-center font-kufam font-semibold gap-3 relative">
+        <span className="text-5xl">+20 </span>
+        <span className="text-3xl">horas de</span>
+        <span className="text-3xl text-primary">Streaming</span>
+      </h3>
+    );
+  }
+  
