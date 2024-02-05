95436ee5d212bb2da50e6dae29f675224788d4aa
daraletdev <daraletdev@gmail.com>
2024-02-04 22:34:04 -0300

    feat: add text below counter (#8)

---
+++ src/components/counterSubtitle.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CounterSubtitle = () => {
+    return (
+        <div className="space-y-5">
+            <h3 className="font-museo text-center text-3xl font-bold">¡Ayúdanos a levantar Chile!</h3>
+            <p className="font-extralight text-center">
+                Recaudaremos fondos que serán entregados a <a href="https://www.instagram.com/p/C243rhlsLsb/?hl=es" target="_blank" rel="noopener noreferrer" className="cursor-pointer hover:text-primary duration-300 font-semibold underline">Bomberos de Chile</a> para
+                ayudar a mitigar los efectos causados por los incendios que comenzaron a
+                ocurrir el 02-02-2024 en la V Región de Chile.
+            </p>
+        </div>
+    );
+};
+
+export default CounterSubtitle;
