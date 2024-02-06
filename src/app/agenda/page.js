affd52bcb1ab9b294748688642da31134ed97ed5
Francisca Medina Concha <fbmedinaconcha@gmail.com>
2024-02-06 02:31:49 -0300

    added 2 timelines based on days

--- src/app/agenda/page.js
+++ src/app/agenda/page.js
@@ -4,6 +4,8 @@ import { Timeline } from "@/components/Timeline";
 import talksData from "@/talks/talks.json";
 
 export default function Agenda() {
+  const talksDay1 = talksData.filter(item => new Date(item.startHour).getDate() === 9);
+  const talksDay2 = talksData.filter(item => new Date(item.startHour).getDate() === 10);
   return (
     <main>
      <Title logo={<svg aria-label="TechTon" className="fill-primary h-32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 376 116">
@@ -12,7 +14,8 @@ export default function Agenda() {
         <AgendaLogo/>
       </svg>} />
 
-      <Timeline data={ talksData } />
+        <Timeline data={talksDay1} />
+        <Timeline data={talksDay2} />
     </main>
   )
 }
