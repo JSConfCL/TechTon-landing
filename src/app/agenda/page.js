7d7af69e0086973eaf3ee32d039c57a50efc9b2a
Francisca Beatriz Medina Concha <fbmedinaconcha@gmail.com>
2024-02-06 00:29:18 -0300

    Feature/agenda (#65)
    * Create agenda page
    
    * referenced agenda button to agenda

---
+++ src/app/agenda/page.js
@@ -0,0 +1,14 @@
+import AgendaLogo from "@/components/AgendaLogo";
+import { Title } from "@/components/Title";
+
+export default function Agenda() {
+  return (
+    <main>
+     <Title logo={<svg aria-label="TechTon" className="fill-primary h-32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 376 116">
+        <title>TechTon | Agenda </title>
+        <desc>Logo de TechTon</desc>
+        <AgendaLogo/>
+      </svg>} />
+    </main>
+  )
+}
