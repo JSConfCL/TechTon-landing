aae2695d8551420d653024bda8d6b2f37db3e530
daraletdev <daraletdev@gmail.com>
2024-02-06 13:58:16 -0300

    feature: Agregar botones a página de donar (#72)

--- src/app/donar/page.js
+++ src/app/donar/page.js
@@ -1,6 +1,8 @@
+import { Button } from "@/components/Button";
 import { DonarSubtitulo } from "@/components/DonarSubtitulo";
 import Logo from "@/components/Logo";
 import { Title } from "@/components/Title";
+import Image from "next/image";
 
 export default function Donar() {
   return (
@@ -20,12 +22,25 @@ export default function Donar() {
         }
       />
       <div className="flex">
-        <div className="py-10 md:space-y-10 space-y-5">
+        <div className="py-10 md:space-y-10 space-y-5 px-10">
           <DonarSubtitulo />
           <div className="font-light leading-7 text-center md:text-left text-sm md:text-lg">
             Todo el dinero será dirigido a Bomberos de Chile. El botón de donar
             te llevará a Mercado Pago y está disponible para donaciones en
             países donde esté habilitado.
+          </div>
+          <div className="flex flex-col gap-3 pt-4 mx-10 sm:flex-row justify-center md:justify-start md:mx-0 md:gap-10">
+            <Button
+              id="button-donate"
+              href="https://link.mercadopago.cl/jscl"
+              target="_blank"
+              variant="primary"
+            >
+              Donar
+            </Button>
+            <Button id="button-info" href="agenda" variant="secondary">
+              Agenda
+            </Button>
           </div>
         </div>
       </div>
