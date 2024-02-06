c21d22abc0c43641cfa30ac8dc42147e572ec56a
Francisca Beatriz Medina Concha <fbmedinaconcha@gmail.com>
2024-02-06 01:28:38 -0300

    agenda timeline base (#77)
    * Added Tailwinds timeline by default
    
    * className instead of class in timeline component
    
    * Added photos to timeline
    
    * remove unused classes in timeline
    
    * added padding and 2px border to timeline
    
    * Update Timeline.jsx
    
    * Update style of speaker photo in timeline
    
    * Update timeline with spaces to hours, speaker name, talk name and community
    
    * Update timeline withh component Image and no comillas in talk name

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+
+export function Timeline() {
+    return (
+        <>
+            <ol className="relative border-s-2 py-10">
+                <li className="mb-10 ms-8">
+                    <span className="absolute mt-2 flex items-center justify-center w-12 h-12 bg-blue-100 rounded-full -start-6">
+                        <Image className="rounded-full" src="images/speakers/agustin-soto.webp" alt="Bonnie image" />
+                    </span>
+                    <time className="mb-1 text-sm font-light leading-none">00:00 - 00:00</time>
+                    <h3 className="text-lg font-bold">Nombre Charlista</h3>
+                    <p className="text-base font-normal">Nombre de la charla</p>
+                    <p className="text-sm font-bold text-gray-400"><a href="url_comunidad" target="_blank">Comunidad</a></p>
+                </li>
+                <li className="mb-10 ms-8">
+                    <span className="absolute mt-2 flex items-center justify-center w-12 h-12 bg-blue-100 rounded-full -start-6">
+                        <Image className="rounded-full" src="images/speakers/agustin-soto.webp" alt="Bonnie image" />
+                    </span>
+                    <time className="mb-1 text-sm font-light leading-none">00:00 - 00:00</time>
+                    <h3 className="text-lg font-bold">Nombre Charlista</h3>
+                    <p className="text-base font-normal">Nombre de la charla</p>
+                    <p className="text-sm font-bold text-gray-400"><a href="url_comunidad" target="_blank">Comunidad</a></p>
+                </li>
+                <li className="mb-10 ms-8">
+                    <span className="absolute mt-2 flex items-center justify-center w-12 h-12 bg-blue-100 rounded-full -start-6">
+                        <Image className="rounded-full" src="images/speakers/agustin-soto.webp" alt="Bonnie image" />
+                    </span>
+                    <time className="mb-1 text-sm font-light leading-none">00:00 - 00:00</time>
+                    <h3 className="text-lg font-bold">Nombre Charlista</h3>
+                    <p className="text-base font-normal">Nombre de la charla</p>
+                    <p className="text-sm font-bold text-gray-400"><a href="url_comunidad" target="_blank">Comunidad</a></p>
+                </li>
+            </ol>
+        </>
+    );
+}
