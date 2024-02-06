e821dda628bd2f6abdc073e6367ac32d5387c948
Denisse Alfaro <83615859+daraletdev@users.noreply.github.com>
2024-02-06 17:30:50 -0300

    feat: Agregar placeholder de codigo QR a página de donar (#73) (#115)

--- src/app/layout.js
+++ src/app/layout.js
@@ -37,9 +37,7 @@ export default function RootLayout({ children }) {
         <div className="hidden lg:block mt-8 mr-10">
           <TwitchButton />
         </div>
-        {/* TODO: Put the twitch button on the right of the screen in desktop here! */}
         <main className="px-3 lg:px-64 pb-12 lg:-mt-16">{children}</main>
-        {/* TODO: Put the footer here! */}
         <Footer />
       </body>
     </html>
