2f3a5c9e0a55a560abf20da6848a0b7775c40630
ceciliaGeraldo <cgeraldoq@gmail.com>
2024-02-07 00:35:54 -0300

    chore: remove commented code and rename json logo UXF

--- src/app/nosotros/page.js
+++ src/app/nosotros/page.js
@@ -18,7 +18,7 @@ export default function Nosotros() {
       />`
     );
   });
-  
+
 
   return (
     <section className="flex flex-col items-center justify-between mx-auto md:w-4/5 lg:w-3/4 xl:w-2/3">
@@ -39,66 +39,46 @@ export default function Nosotros() {
         <div className="py-10 md:space-y-10 space-y-5 flex-1">
           <div className="font-bold text-left xl:text-left">
             <p className="text-2xl lg:text-3xl">
-             ¿De qué se trata?
+              ¿De qué se trata?
             </p>
           </div>
           <div className="font-light leading-7 text-center md:text-left text-sm md:text-lg">
-                Somos varias comunidades tech unidas bajo el alero de <a
-          href="https://jschile.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="cursor-pointer hover:text-primary duration-300 font-semibold underline"
-        >
-          Javascript Chile
-        </a>{" "}
-        para ayudar a mitigar los efectos causados por los incendios que
-        comenzaron a ocurrir el 02-02-2024 en la V Región de Chile., que brindaremos charlas y workshops gratuitos mediante streaming para recaudar fondos que serán entregados a <a
-          href="https://www.instagram.com/p/C243rhlsLsb/?hl=es"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="cursor-pointer hover:text-primary duration-300 font-semibold underline"
-        >
-          Bomberos de Chile
-        </a>{" "}
-        para ayudar a mitigar los efectos causados por los incendios que
-        comenzaron a ocurrir el 02-02-2024 en la V Región de Chile. y así poder ayudar a paliar los efectos causados por los incendios forestales que comenzaron a ocurrir el 02-02-2024 en la V Región de Chile (Valparaíso)
+            Somos varias comunidades tech unidas bajo el alero de <a
+              href="https://jschile.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer hover:text-primary duration-300 font-semibold underline"
+            >
+              Javascript Chile
+            </a>{" "}
+            para ayudar a mitigar los efectos causados por los incendios que
+            comenzaron a ocurrir el 02-02-2024 en la V Región de Chile., que brindaremos charlas y workshops gratuitos mediante streaming para recaudar fondos que serán entregados a <a
+              href="https://www.instagram.com/p/C243rhlsLsb/?hl=es"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer hover:text-primary duration-300 font-semibold underline"
+            >
+              Bomberos de Chile
+            </a>{" "}
+            para ayudar a mitigar los efectos causados por los incendios que
+            comenzaron a ocurrir el 02-02-2024 en la V Región de Chile. y así poder ayudar a paliar los efectos causados por los incendios forestales que comenzaron a ocurrir el 02-02-2024 en la V Región de Chile (Valparaíso)
           </div>
           <div className="font-light text-primary leading-7 text-center md:text-left text-sm md:text-lg">
-                Las donaciones se harán a una cuenta única y luego haremos una transferencia con el total de fondos recaudados.
+            Las donaciones se harán a una cuenta única y luego haremos una transferencia con el total de fondos recaudados.
           </div>
         </div>
-        <div className="space-y-10 flex-3">
-        {CommunitiesLogo.map((logo, key) => {
-        return (<Image
-        className="ml-2"
-        id={key}
-        alt={logo.alt}
-        src={logo.src}
-        height={100}
-        width={100}
-      />)}
-        )}
-        {/* <Image
-                className="ml-2"
-                alt="Hora Chile"
-                src="images/communities/9punto5.webp"
-                height={100}
-                width={100}
-            />
-            <Image
-                className="ml-2"
-                alt="Hora Chile"
-                src="images/communities/devschile2018.webp"
-                height={100}
-                width={100}
-            />
-            <Image
-                className="ml-2"
-                alt="Hora Chile"
-                src="images/communities/El-Club-de-la-IA.webp"
-                height={100}
-                width={100}
-            /> */}
+        <div className="">
+          {CommunitiesLogo.map((logo, key) => {
+            return (<Image
+              className="ml-2"
+              id={key}
+              alt={logo.alt}
+              src={logo.src}
+              height={100}
+              width={100}
+            />)
+          }
+          )}
         </div>
       </div>
     </section>
