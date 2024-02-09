5ae6b8fe04ca868a08f366eb9ed1ca2e80b1abf7
Jose Lezama <me@joseglego.io>
2024-02-09 11:47:15 -0300

    fix: apply rules for mobiles

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -20,13 +20,13 @@ const Card = ({
         e.target.scrollIntoView({ behavior: "smooth", block: "center" });
       }}
       className={cn(
-        "w-[420px] min-h-[160px] h-fit relative flex flex-col mx-4 p-5 gap-5 bg-[#3F3F3F] rounded-lg shadow-xl cursor-pointer",
+        "w-[380px] md:w-[420px] min-h-[160px] h-fit relative flex flex-col mx-4 p-5 gap-5 bg-[#3F3F3F] rounded-lg shadow-xl cursor-pointer self-start",
         {
-          "self-end": leftSide,
-          "self-start": !leftSide,
+          "lg:self-end": leftSide,
+          "lg:self-start": !leftSide,
           "opacity-75": status === "past",
           "bg-[#9146FF] text-white": status === "current",
-          "text-right": leftSide,
+          "lg:text-right": leftSide,
         },
         {
           "": true,
@@ -49,7 +49,7 @@ const Card = ({
       </time>
       <div
         className={cn("flex flex-row gap-4 items-center", {
-          "flex-row-reverse": leftSide,
+          "lg:flex-row-reverse": leftSide,
         })}
       >
         <span className="p-[2px] flex items-center justify-center w-12 h-12 bg-blue-100 rounded-full -start-6">
@@ -84,9 +84,9 @@ const Card = ({
 
 export function Timeline({ data }) {
   return (
-    <motion.div className="flex flex-col w-full items-center pb-24">
-      <ol className="flex justify-center items-center flex-col gap-4 relative w-1 py-10 pb-24">
-        <div className="w-[5px] rounded-xl h-full bg-white absolute top-0 left-1/2 transform -translate-x-1/2" />
+    <motion.div className="flex flex-col w-full items-start lg:items-center pb-24">
+      <ol className="flex justify-center items-start -left-[calc(50vw_-8px)] lg:left-0 lg:items-center flex-col gap-4 relative w-1 py-10 pb-24">
+        <div className="w-[5px] rounded-xl h-full bg-white absolute top-0 lg:left-1/2 lg:transform lg:-translate-x-1/2" />
         {data.map((item, index) => {
           return (
             <Card
