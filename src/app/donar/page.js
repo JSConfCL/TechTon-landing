52f2a03a967377da00304809590dad817c28c822
Jose Lezama <me@joseglego.io>
2024-02-09 11:04:38 -0300

    Lego/some styling (#245)
    * core: define API to get values
    
    * core: include tan-stack for pooling and query cache
    
    * feat: update doante page with API info

--- src/app/donar/page.js
+++ src/app/donar/page.js
@@ -16,29 +16,30 @@ export const generateViewport = () => getViewports();
 
 export default function Donar() {
   return (
-    <>
+    <div className="flex flex-1 overflow-hidden relative">
       <FadeInBackgroundImages
         className="bg-no-repeat bg-cover bg-left absolute w-[50%] aspect-square right-0 top-0 shadow-inner -z-10 lg:block"
         image={bomberos}
       />
       <div className="flex h-full w-full flex-col relative justify-center items-center max-w-full md:px-32 xl:max-w-[1920px] xl:px-5 m-auto xl:justify-center">
-        <div className="flex flex-col xl:flex-row gap-4 w-full flex-1 xl:px-24 px-8 pt-20">
+        <div className="flex flex-col xl:flex-row gap-4 w-full flex-1 px-8 xl:pl-40 pt-20">
           <div className="flex flex-col flex-1 gap-8 justify-end ">
             <div className="text-center font-bold xl:text-left flex flex-col gap-4">
               <h2 className="text-5xl xl:text-8xl">Gracias</h2>
-              <p className="text-3xl xl:text-4xl">
-                por <span className="text-primary">sumarte a donar</span> dinero
-                para esta causa.
+              <p className="text-3xl xl:text-[48px] xl:leading-[64px] pr-4">
+                por <span className="text-primary">sumarte a donar</span><br/>
+                dinero para esta causa.
               </p>
             </div>
             <DonarSubtitulo />
           </div>
           <div className="hidden xl:flex flex-1" />
         </div>
-        <div className="flex w-full flex-1 px-20 justify-start">
+        <div className="flex flex-col xl:flex-row gap-4 w-full flex-1 px-8 xl:pl-40  pt-10">
           <DonateInfo />
+          <div className="hidden xl:flex flex-1" />
         </div>
       </div>
-    </>
+    </div>
   );
 }
