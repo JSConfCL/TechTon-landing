0c1723208448bc13fab07191e097f716bc4220fd
Felipe Torres <felipe.torressepulveda@gmail.com>
2024-02-09 04:09:19 -0800

    Timeline fixes

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,44 +1,103 @@
 import Image from "next/image";
+import { useScroll, useAnimation, useInView, motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+const Card = ({
+  item,
+  leftSide,
+  status, // current, future, past
+}) => {
+  const timeFormatter = new Intl.DateTimeFormat("es-ES", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const startTime = timeFormatter.format(new Date(item.startHour));
+  const endTime = timeFormatter.format(new Date(item.endHour));
+
+  return (
+    <motion.li
+      onClick={(e) => {
+        e.target.scrollIntoView({ behavior: "smooth", block: "center" });
+      }}
+      className={cn(
+        "w-[420px] min-h-[160px] h-fit relative flex flex-col mx-4 p-5 gap-5 bg-[#3F3F3F] rounded-lg shadow-xl cursor-pointer",
+        {
+          "self-end": leftSide,
+          "self-start": !leftSide,
+          "opacity-75": status === "past",
+          "bg-[#9146FF] text-white": status === "current",
+          "text-right": leftSide,
+        },
+        {
+          "": true,
+        },
+      )}
+      initial={{
+        opacity: 0.5,
+        scale: 0.8,
+        y: leftSide ? -10 : 10,
+      }}
+      margin="100px"
+      whileInView={{
+        opacity: 1,
+        scale: 1,
+        y: 0,
+      }}
+    >
+      <time className="text-sm font-light leading-none opacity-60">
+        {startTime} - {endTime}
+      </time>
+      <div
+        className={cn("flex flex-row gap-4 items-center", {
+          "flex-row-reverse": leftSide,
+        })}
+      >
+        <span className="p-[2px] flex items-center justify-center w-12 h-12 bg-blue-100 rounded-full -start-6">
+          <Image
+            className="rounded-full aspect-square w-full h-full"
+            src={item.image}
+            alt={item.name}
+            // fill
+            width="48"
+            height="48"
+          />
+        </span>
+        <div className={"flex flex-col justify-center"}>
+          <span className="text-lg font-bold">{item.name}</span>
+          {item.community !== "ninguna" && (
+            <p className="text-sm font-bold text-gray-400">
+              <a href={item.communityUrl} target="_blank">
+                {item.community}
+              </a>
+            </p>
+          )}
+        </div>
+      </div>
+      <p className="text-base font-normal italic">
+        {'"'}
+        {item.topic}
+        {'"'}
+      </p>
+    </motion.li>
+  );
+};
 
 export function Timeline({ data }) {
   return (
-    <>
-      <ol className="relative border-s-2 py-10">
+    <motion.div className="flex flex-col w-full items-center pb-24">
+      <ol className="flex justify-center items-center flex-col gap-4 relative w-1 py-10 pb-24">
+        <div className="w-[5px] rounded-xl h-full bg-white absolute top-0 left-1/2 transform -translate-x-1/2" />
         {data.map((item, index) => {
-          const timeFormatter = new Intl.DateTimeFormat("es-ES", {
-            hour: "2-digit",
-            minute: "2-digit",
-          });
-          const startTime = timeFormatter.format(new Date(item.startHour));
-          const endTime = timeFormatter.format(new Date(item.endHour));
-
           return (
-            <li key={index} className="mb-10 ms-8">
-              <span className="absolute mt-2 flex items-center justify-center w-12 h-12 bg-blue-100 rounded-full -start-6">
-                <Image
-                  className="rounded-full"
-                  src={item.image}
-                  alt={item.name}
-                  width="100"
-                  height="100"
-                />
-              </span>
-              <time className="mb-1 text-sm font-light leading-none">
-                {startTime} - {endTime}
-              </time>
-              <h3 className="text-lg font-bold">{item.name}</h3>
-              <p className="text-base font-normal">{item.topic}</p>
-              {item.community !== "ninguna" && (
-                <p className="text-sm font-bold text-gray-400">
-                  <a href={item.communityUrl} target="_blank">
-                    {item.community}
-                  </a>
-                </p>
-              )}
-            </li>
+            <Card
+              key={index}
+              item={item}
+              leftSide={(index + 1) % 2 === 0}
+              status={"past"}
+            />
           );
         })}
       </ol>
-    </>
+    </motion.div>
   );
 }
