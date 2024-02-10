02272edfb6b6717b975901efec31ac62671836d3
Zyruks <kuzenshi@gmail.com>
2024-02-10 12:01:47 -0400

    Fix: improve responsiveness of donation total display  (#250)
    * fix(component): 🐛 improve responsiveness of donation total display
    
    - Utilize CSS grid for center alignment and consistent spacing.
    - Implement clamp function for dynamic text resizing across devices.
    - Remove explicit line break in favor of gap utility for better spacing control.
    
    This update enhances the mobile and desktop visual presentation of the total donation amount, ensuring a better user experience across all devices.
    
    * style(ui): 💄 enhance layout responsiveness with min-width adjustments
    
    - Add `md:min-w-96` to set a minimum width for medium screen sizes.
    - Use `lg:min-w-fit` to adjust minimum width for larger screens, ensuring content adapts more gracefully.
    
    This modification improves the component's visual stability and appearance on medium and large devices, enhancing the overall user experience.

--- src/components/DonateInfo.jsx
+++ src/components/DonateInfo.jsx
@@ -45,7 +45,7 @@ const DonateButton = ({
   };
 
   return (
-    <div className="mx-auto w-full md:w-auto flex flex-col items-center justify-center gap-4 p-4 text-center bg-[#333] rounded-lg drop-shadow-lg">
+    <div className="mx-auto w-full md:w-auto flex flex-col items-center justify-center gap-4 p-4 text-center bg-[#333] rounded-lg drop-shadow-lg md:min-w-96 lg:min-w-fit">
       <div className="mx-auto flex items-center gap-2 lg:px-4">
         {system === "mercadopago" ? (
           <Image alt="" src="/images/flags/chile.svg" height={35} width={35} />
@@ -104,9 +104,9 @@ const DonateButton = ({
           &nbsp;
         </div>
       ) : (
-        <div className="font-bold text-lg">
-          Total Recolectado<br/>
-          <span className="text-5xl text-primary">{formatMoney(totalDonation, system)}</span>
+        <div className="font-bold text-lg grid justify-center items-center gap-4">
+          Total Recolectado
+          <span className="text-[clamp(1.2rem,10vw,3rem)] text-primary">{formatMoney(totalDonation, system)}</span>
         </div>
       )}
     </div>
