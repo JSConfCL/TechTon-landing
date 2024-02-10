d56183a83d0fbcdb53af3c22f46079946068b4a9
alexeira <alex.pincheira06@gmail.com>
2024-02-10 16:51:00 -0300

    feat: add function for get dollar currency value from api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,8 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "https://api.jschile.org/graphql";
+const SPECIAL_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSx9_AO3FFM9QN4QKgxcueQvmpfE8L4Z3CBlqJ3uGQ1EJaG9Nt9Nm-lafR_30Aj5wnvd58gpqn47u8Z/pub?output=tsv'
 const DATE = "2024-02-05T00:00:00.000Z";
+
 const graphqlQuery = {
   query: `
     query q {
@@ -31,3 +33,26 @@ export const getTransactions = async () => {
     .then((json) => json.data)
     .catch((error) => console.error("Error:", error));
 };
+
+export const getSpecialDonations = async () => {
+  const csv = await fetch(SPECIAL_URL)
+    .then(res => res.text())
+
+  return csv
+    .split('\n')
+    .slice(1)
+    .map(row => {
+      const [title, donation] = row.split('\t')
+      const parsedRow = { title, donation: +donation.replace('\r', '') }
+
+      return parsedRow
+    })
+}
+
+export async function getDollarCurrency() {
+  const res = await fetch("https://mindicador.cl/api/dolar");
+  const data = await res.json();
+  const lastPossibleValue = data?.serie[0].valor;
+
+  return lastPossibleValue;
+}
