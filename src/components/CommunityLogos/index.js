f9498c4db325e24f5cefa51a3dbfb377315ce26c
Edgardosilva <edgardosilva.es@gmail.com>
2024-02-10 16:18:31 -0300

    Sea gregan links a logos de comunidades

--- src/components/CommunityLogos/index.js
+++ src/components/CommunityLogos/index.js
@@ -3,6 +3,12 @@ import CommunitiesLogo from "@/communities/communities.json";
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 
+
+const goToImgUrl = (e) => {
+  const url = e.currentTarget.getAttribute('url');
+  window.open(url, '_blank');
+};
+
 export const CommunityLogos = () => (
   <AnimatePresence mode="popLayout">
     {[...CommunitiesLogo.entries()]
@@ -18,10 +24,12 @@ export const CommunityLogos = () => (
             transition={{ duration: 0.25, delay }}
           >
             <Image
-              className="p-4"
+              className="p-4 cursor-pointer transition transform hover:scale-150"
               key={key}
               alt={logo.alt}
               src={logo.src}
+              url={logo.url}
+              onClick={goToImgUrl}
               fill
               objectFit="contain"
             />
